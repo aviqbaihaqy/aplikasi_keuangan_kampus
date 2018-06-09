e97ebe228fa8ec8b7b8aa7cf40913bc46db8def3
flashdin <flashdin007@gmail.com>
2018-06-09 14:19:17 +0700

    update file

--- laravel/database/migrations/2018_06_09_061930_create_penagihan_piutang.php
+++ laravel/database/migrations/2018_06_09_061930_create_penagihan_piutang.php
@@ -16,10 +16,10 @@ class CreatePenagihanPiutang extends Migration
         Schema::create('penagihan_piutang', function (Blueprint $table) {
             $table->increments('id_penagihan_piutang');
             $table->date('tgl_penagihan_piutang');
-            $table->string('no_tagihan_piutang');
-            $table->string('ditagih_kpd');
-            $table->string('keterangan');
-            $table->double('total');
+            $table->string('no_tagihan_piutang', 50);
+            $table->string('ditagih_kpd', 50);
+            $table->string('keterangan', 50);
+            $table->integer('total');
         });
     }
 
