9d2067fdb5d13ec90f7585d77253e1a655003d4b
flashdin <flashdin007@gmail.com>
2018-07-06 20:05:08 +0700

    update file penagihan_piutan.blade.php

--- laravel/resources/views/penagihan_piutang.blade.php
+++ laravel/resources/views/penagihan_piutang.blade.php
@@ -362,16 +362,9 @@
                                             </div>
                                             <div class="form-group col-md-2">
                                                 <select id="select1" name="select1" class="form-control">
-                                                    <option value="1">01</option>
-                                                    <option value="2">02</option>
-                                                    <option value="3">03</option>
-                                                    <option value="4">04</option>
-                                                    <option value="5">05</option>
-                                                    <option value="6">06</option>
-                                                    <option value="7">07</option>
-                                                    <option value="8">08</option>
-                                                    <option value="9">09</option>
-                                                    <option value="10">10</option>
+                                                    @for ($i = 1; $i < 11; $i++)
+                                                        <option value="{{ $i }}">{{ $i }}</option>
+                                                    @endfor
                                                 </select>
                                             </div>
                                             <div class="form-group col-md-2">
@@ -412,16 +405,9 @@
                                             </div>
                                             <div class="form-group col-md-2">
                                                 <select id="select1" name="select1" class="form-control">
-                                                    <option value="1">01</option>
-                                                    <option value="2">02</option>
-                                                    <option value="3">03</option>
-                                                    <option value="4">04</option>
-                                                    <option value="5">05</option>
-                                                    <option value="6">06</option>
-                                                    <option value="7">07</option>
-                                                    <option value="8">08</option>
-                                                    <option value="9">09</option>
-                                                    <option value="10">10</option>
+                                                    @for ($i = 1; $i < 11; $i++)
+                                                        <option value="{{ $i }}">{{ $i }}</option>
+                                                    @endfor
                                                 </select>
                                             </div>
                                             <div class="form-group col-md-2">
