72fdd166f026b3e291aa5e06985f6f677dceb2c6
kuscatur <38804819+kuscatur@users.noreply.github.com>
2018-07-10 14:39:19 +0700

    Add files via upload

---
+++ laravel/resources/views/laporanpemakaianbarangblade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--
+* CoreUI - Free Bootstrap Admin Template
+* @version v2.0.0-beta.0
+* @link https://coreui.io
+* Copyright (c) 2018 creativeLabs Łukasz Holeczek
+* Licensed under MIT (https://coreui.io/license)
+-->
+
+<html lang="en">
+    <head>
+	<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    	<title>Menambah Laporan Pemakaian Barang</title>
+    	<style type="text/css">
+    		.table {
+    			border-collapse: collapse;
+    		}
+    		td {
+    			border-top: 1px solid #000;
+    			border-left: 1px solid #000;
+    			border-right: 1px solid #000;
+    			border-bottom: 1px solid #000;
+    			padding: 3px 4px;
+    		}
+    		.no-border {
+    			border-right: 1px solid transparent;
+    		}
+    	</style>
+  	</head>
+  	
+	<body>
+    <h1>Sistem Informasi Keuangan</h1>
+		<h1>Menambah Laporan Pemakaian Barang</h1>
+			<br><br>			
+                                        
+                                    <table class="table" width="100%">
+                                        <thead align="center">
+										       <td>NO</th>
+                                                <td>TANGGAL</td>
+                                                <td>NOMOR</td>
+                                                <td>SUPPLIER</td>
+                                                <td>BAGIAN</td>
+                                                <td>TOTAL (RP.)</td>
+                                                <td>AKSI</td>
+                                            </tr>
+                                        </thead>
+                                        <tbody align="center">
+                                            <tr>
+                                                <td>1</td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+												
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <!--/.col-->
+                    </div>
+                </div>
+            </main>
+            
+        <!-- Bootstrap and necessary plugins-->
+        <script src="assets/vendors/js/jquery.min.js"></script>
+        <script src="assets/vendors/js/popper.min.js"></script>
+        <script src="assets/vendors/js/bootstrap.min.js"></script>
+        <script src="assets/vendors/js/pace.min.js"></script>
+        <script src="assets/vendors/js/perfect-scrollbar.min.js"></script>
+        <script src="assets/vendors/js/coreui.min.js"></script>
+    </body>
+</html>
